2aff9b33410696725f87dc408ac4284eb9495940
SrNexar <nfsalguero@uce.edu.ec>
2024-11-13 11:44:08 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+
+	server := gin.Default()
+
+	server.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "Hola desde el Lenguaje GO!!!",
+		})
+	})
+
+	server.Run()
+}
